48cc88f9e870f9a4fd4cc4ef2e78b182a622972b
Komronbek <ochilovkomronbek809@gmail.com>
2024-11-05 10:29:08 +0500

    totel

--- cart/cart.py
+++ cart/cart.py
@@ -32,6 +32,22 @@ class Cart():
 
         self.session.modified = True
     
+    def cart_total(self):
+        #get product IDS
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        quantities = self.cart
+        total = 0
+        for key, value in quantities.items():
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else: 
+                        total = total + (product.price * value)
+        return total
+
     def __len__(self):
         return len(self.cart)
 
