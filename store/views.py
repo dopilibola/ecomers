62216c491ccc8dea9b22c7198d329e0526494141
Komronbek <ochilovkomronbek809@gmail.com>
2024-11-07 10:14:56 +0500

    update user

--- store/views.py
+++ store/views.py
@@ -5,8 +5,27 @@ from django.contrib import messages
 from django.shortcuts import redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
-from .forms import SignUpForm
+from .forms import SignUpForm, UpdateUserForm
 from django import forms
+
+
+def update_user(request):
+    if request.user.is_authenticated:
+        current_user = User.objects.get(id=request.user.id)
+        user_form = UpdateUserForm(request.POST or None, instance=current_user )
+
+        if user_form.is_valid():
+            user_form.save()
+
+            login(request, current_user)
+            messages.success(request, "User Has Been Update ")
+            return redirect('home')
+
+        return render(request, "update_user.html", {'user_form': user_form})
+    else:
+        messages.success(request, "you must be logged in to access that page! ")
+        return redirect('home')
+    # return render(request, 'update_user.html', {})
 
 
 def category_summary(request):
