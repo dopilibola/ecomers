29195a5351c1ff95e6486a9916cdf8a2cc5e3055
Komronbek <ochilovkomronbek809@gmail.com>
2024-11-08 12:10:35 +0500

    ProFile

--- store/admin.py
+++ store/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from .models import Category, Customer, Product, Order
+from .models import Category, Customer, Product, Order, Profile
+from django.contrib.auth.models import User
 
 admin.site.register(Category)
 admin.site.register(Product)
 admin.site.register(Customer)
 admin.site.register(Order)
+admin.site.register(Profile)
+
+# Mix profile info and user info 
+class ProfileInline(admin.StackedInline):
+    model = Profile
+
+
+
+# extend user model
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    field = ["username", "first_name", "last_name", "email"]
+    inlines = [ProfileInline]
+
+# unregister the old wey
+admin.site.unregister(User)
+
+# Re-register the new way
+admin.site.register(User, UserAdmin)
