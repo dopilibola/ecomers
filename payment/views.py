0844dd6f74c2857580ea6ca894d6c8c705611134
Komronbek <ochilovkomronbek809@gmail.com>
2024-11-11 19:00:10 +0500

    checkout

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from cart.cart import Cart
+
+def checkout(request):
+    
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+    return render(request, "checkout.html", {"cart_products":cart_products, "quantities":quantities, "totals":totals})
+
+    
 
 def payment_success(request):
 
